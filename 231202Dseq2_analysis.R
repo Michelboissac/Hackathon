4eb2a17f6cbfadc9df2474b7338bb04f26c40af0
Kamenan N'gadi <111643722+KamenanN@users.noreply.github.com>
2023-12-04 01:29:15 +0100

    Add files via upload

--- 231202Dseq2_analysis.R
+++ 231202Dseq2_analysis.R
@@ -13,78 +13,67 @@
 library("DESeq2")
 library("pheatmap")
 library("RColorBrewer")
+library("ggpubr")
+
 
 
 #1#################Creation des tableau cts et coldata necessaire pour deseq2
-tab1=read.table("output_dir/SRR10379721_trimmed.bam.count.txt",head = TRUE)
-tab2=read.table("output_dir/SRR10379722_trimmed.bam.count.txt",head = TRUE)
-tab3=read.table("output_dir/SRR10379723_trimmed.bam.count.txt",head = TRUE)
-tab4=read.table("output_dir/SRR10379724_trimmed.bam.count.txt",head = TRUE)
-tab5=read.table("output_dir/SRR10379725_trimmed.bam.count.txt",head = TRUE)
-tab6=read.table("output_dir/SRR10379726_trimmed.bam.count.txt",head = TRUE)
+#dir = "/home/kngadi/Documents/outputfeaturecounts_inputDeseq2(-s 1)/"
+#setwd(dir = dir)
 
-cts=data.frame(tab1$SRR10379721_trimmed.bam,
-               tab2$SRR10379722_trimmed.bam,
-               tab3$SRR10379723_trimmed.bam,
-               tab4$SRR10379724_trimmed.bam,
-               tab5$SRR10379725_trimmed.bam,
-               tab6$SRR10379726_trimmed.bam)
+count_matrix <- function(){
+  #set working direcroty 
+  #setwd(dir = dir)
+  # # Créer une chaine de caractères avec les fichier à extension .txt selon le repertoire de travail
+  files_txt <- list.files( pattern = "\\.txt$")
+  ## stocker les dataframe de samples dans une liste en gardant que le Geneid et les featurecounts
+  data_list <- lapply(files_txt, function(x)read.table(x, skip = 1)[, c("V1", "V8")])
+  # Fusionner par le Geneid
+  df <- plyr::join_all(data_list, by= "V1", type = "inner") 
+  # Remplacer la première colonnes par la première ligne
+  names(df) <- as.matrix(df[1, ]) # selectionner la première colonne
+  df <- df[-1, ] # supprimer celle-ci
+  # Convertir les colonnes dans leur type appropriés
+  df[] <- lapply(df, function(x) type.convert(as.character(x))) 
+  rownames(df) <- NULL
+  # Construction de la matrice de comptage 
+  cts <- data.frame(df, row.names = 1) # Remplacer l'index par l'identifiants des gênes pour obtenir la matrice de comptage
+  #Ordonner les sampples selon le tableau de reference pour comparaison
+  cts <- with(data = cts, expr = cts[,order(c(4,5,6,1,2,3))])
+  colnames(cts) <- c("ctrl4", "ctrl5","ctrl6", "IP1", "IP2", "IP3")
+  return(cts)
+}
+Deseq2_object <- function(cts, thrsd){
+  # Le facteur de design
+  smple_trimmed <- colnames(cts)
+  sample_infos = data.frame(annot=smple_trimmed,
+                            Staphylococcus_aureus=as.factor(c( "treated", 
+                                                               "treated", 
+                                                               "treated", 
+                                                               "untreated",
+                                                               "untreated", 
+                                                               "untreated")),
+                            row.names = 1
+                          ) 
+  cts <- na.omit(cts)
+  dds <- DESeqDataSetFromMatrix(countData = cts,
+                                colData = sample_infos,
+                                design = ~ Staphylococcus_aureus
+  )
+  dds$Staphylococcus_aureus <- relevel(dds$Staphylococcus_aureus, ref = "untreated")
+  # Exécuter DEsq
+  dds <- DESeq(dds)
+  #Explorer les résultats
+  res <-results(dds, alpha = thrsd)
+  return(list(res=res,dds=dds))
+  
+}
 
-rownames(cts)=tab1$Name
-x=c("tab1.SRR10379721_trimmed.bam",
-    "tab2.SRR10379722_trimmed.bam",
-    "tab3.SRR10379723_trimmed.bam",
-    "tab4.SRR10379724_trimmed.bam",
-    "tab5.SRR10379725_trimmed.bam",
-    "tab6.SRR10379726_trimmed.bam"
-)
-
-coldata=data.frame(condition = x)
-rownames(coldata)=coldata$condition
-coldata$condition=c("treated","untreated")
-coldata$type=c("1","2","3","1","2","3")
-colnames(coldata)=c("condition","type")
-
-coldata$condition = factor(coldata$condition)
-coldata$type = factor(coldata$type)
-
-
-#2#################################Analyse Deseq2
-dds <- DESeqDataSetFromMatrix(countData = cts,
-                              colData = coldata,
-                              design= ~ type + condition)
-
-
-dds <- DESeq(dds)
-resultsNames(dds) # lists the coefficients
-res <- results(dds)
-summary(res)
+dds <- dsq_objt$dds
+res<- dsq_objt$res
+  # Plot Ma-plot
 png(file = "plotMA.png", width = 800, height = 700)
-plotMA(res, ylim=c(-2,2))
-dev.off()
-
-
-keep <- rowSums(counts(dds)) >=10
-dds <- dds[keep,]
-# Spécifier le facteur de niveau
-dds$condition <- relevel(dds$condition, ref = "untreated")
-# Exécuter DEsq
-dds <- DESeq(dds)
-res <- results(dds)
-res05 <-results(dds, alpha = 0.05)
-# Explore results
-summary(res05)
-# MA plot
-
-png(file = "plotres05.png", width = 800, height = 700)
-plotMA(res05)
-dev.off()
-
-
-png(file = "plotCounts.png", width = 800, height = 700)
-plotCounts(dds = dds,
-           gene = which.min(res$padj),
-           intgroup = "condition")
+plotMA(res, alpha=0.05, colNonSig="black", colSig="red")
 dev.off()
 
 ntd <- normTransform(dds)
@@ -96,18 +85,14 @@ sampleDistMatrix <- as.matrix(sampleDists)
 rownames(sampleDistMatrix) <- paste(vsd$condition, sep="-")
 colnames(sampleDistMatrix) <- NULL
 colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
-
+# clustrering hierachique
 png(file = "pheatmap.png", width = 800, height = 700)
-
 pheatmap(sampleDistMatrix,
          clustering_distance_rows=sampleDists,
          clustering_distance_cols=sampleDists,
          col=colors)
 dev.off()
 
-
 png(file = "plotPCA.png", width = 800, height = 700)
-plotPCA(vsd, intgroup=c("condition"))
+plotPCA(vsd, intgroup=c("Staphylococcus_aureus"))
 dev.off()
-
-#############################################################################
